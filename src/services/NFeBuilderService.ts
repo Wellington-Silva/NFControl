9e00fc5c4cb006c2309a10f383ec4ed2181500fd
Wellington-Silva <wellingtonsilva112000@gmail.com>
2025-08-06 21:05:55 -0300

    GenerateNFSXML

--- src/services/NFeBuilderService.ts
+++ src/services/NFeBuilderService.ts
@@ -1,5 +1,41 @@
 import { create } from 'xmlbuilder2';
 
+interface NfseData {
+    prestador: {
+        cnpj: string;
+        inscricaoMunicipal: string;
+    };
+    rps: {
+        numero: string;
+        serie: string;
+        tipo: string;
+        dataEmissao: string;
+        naturezaOperacao: string;
+        regimeTributacao: string;
+        simplesNacional: string;
+        incentivador: string;
+    };
+    servico: {
+        valor: string;
+        issRetido: string;
+        valorIss: string;
+        itemListaServico: string;
+        discriminacao: string;
+        codigoMunicipio: string;
+    };
+    tomador: {
+        cnpj?: string;
+        cpf?: string;
+        razaoSocial: string;
+        endereco: string;
+        numero: string;
+        bairro: string;
+        codigoMunicipio: string;
+        uf: string;
+        cep: string;
+    };
+};
+
 class GenerateXML {
 
     async generateNFeXML(invoiceData: any) {
@@ -152,10 +188,69 @@ class GenerateXML {
         console.log(xml);
     };
 
+    async generateNfseXML(data: NfseData): Promise<string> {
+        const xml = create({ version: '1.0', encoding: 'UTF-8' })
+            .ele('EnviarLoteRpsEnvio', { xmlns: 'http://www.abrasf.org.br/nfse.xsd' })
+            .ele('LoteRps', { Id: 'lote1', versao: '2.04' })
+                .ele('NumeroLote').txt('1').up()
+                .ele('Cnpj').txt(data.prestador.cnpj).up()
+                .ele('InscricaoMunicipal').txt(data.prestador.inscricaoMunicipal).up()
+                .ele('QuantidadeRps').txt('1').up()
+                .ele('ListaRps')
+                .ele('Rps')
+                    .ele('InfRps', { Id: `rps${data.rps.numero}` })
+                    .ele('IdentificacaoRps')
+                        .ele('Numero').txt(data.rps.numero).up()
+                        .ele('Serie').txt(data.rps.serie).up()
+                        .ele('Tipo').txt(data.rps.tipo).up()
+                    .up()
+                    .ele('DataEmissao').txt(data.rps.dataEmissao).up()
+                    .ele('NaturezaOperacao').txt(data.rps.naturezaOperacao).up()
+                    .ele('RegimeEspecialTributacao').txt(data.rps.regimeTributacao).up()
+                    .ele('OptanteSimplesNacional').txt(data.rps.simplesNacional).up()
+                    .ele('IncentivadorCultural').txt(data.rps.incentivador).up()
+                    .ele('Status').txt('1').up()
+                    .ele('Servico')
+                        .ele('Valores')
+                        .ele('ValorServicos').txt(data.servico.valor).up()
+                        .ele('IssRetido').txt(data.servico.issRetido).up()
+                        .ele('ValorIss').txt(data.servico.valorIss).up()
+                        .up()
+                        .ele('ItemListaServico').txt(data.servico.itemListaServico).up()
+                        .ele('Discriminacao').txt(data.servico.discriminacao).up()
+                        .ele('CodigoMunicipio').txt(data.servico.codigoMunicipio).up()
+                    .up()
+                    .ele('Prestador')
+                        .ele('Cnpj').txt(data.prestador.cnpj).up()
+                        .ele('InscricaoMunicipal').txt(data.prestador.inscricaoMunicipal).up()
+                    .up()
+                    .ele('TomadorServico')
+                        .ele('IdentificacaoTomador')
+                        .ele('CpfCnpj')
+                            .ele(data.tomador.cnpj ? 'Cnpj' : 'Cpf')
+                            .txt(data.tomador.cnpj || data.tomador.cpf!)
+                            .up()
+                        .up()
+                        .up()
+                        .ele('RazaoSocial').txt(data.tomador.razaoSocial).up()
+                        .ele('Endereco')
+                        .ele('Endereco').txt(data.tomador.endereco).up()
+                        .ele('Numero').txt(data.tomador.numero).up()
+                        .ele('Bairro').txt(data.tomador.bairro).up()
+                        .ele('CodigoMunicipio').txt(data.tomador.codigoMunicipio).up()
+                        .ele('Uf').txt(data.tomador.uf).up()
+                        .ele('Cep').txt(data.tomador.cep).up()
+                        .up()
+                    .up()
+                    .up()
+                .up()
+                .up()
+            .up()
+            .end({ prettyPrint: true });
 
-    async generateNFSeXML(invoiceData: any) {
-        
+        return xml;
     };
+    
 };
 
 export default new GenerateXML();
